fbeab71762c8048adb0e6ad2d965aa746f56e82f
Hammouche-Med <mhammouche995@gmail.com>
2022-08-02 11:16:32 +0100

    update

--- users/models.py
+++ users/models.py
@@ -1,12 +1,14 @@
+from statistics import mode
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User (AbstractUser):
-    username = None
+    username = models.CharField(max_length=254, unique=True, null=True)
     email = models.EmailField(unique=True)
+    phone = models.IntegerField(unique=True, null=True)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name', 'phone']
 
     def __str__(self):
         return self.email
