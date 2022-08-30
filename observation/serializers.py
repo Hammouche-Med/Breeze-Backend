8bb2afd46cbadac332e40d88849949cebba9b560
Hammouche-Med <mhammouche995@gmail.com>
2022-08-30 22:58:13 +0100

    update

--- observation/serializers.py
+++ observation/serializers.py
@@ -3,6 +3,8 @@ from .models import Observation
  
 class ObservationSerializer( serializers.ModelSerializer) :
     station_name = serializers.CharField(source='station.name', read_only=True)
+    station_oaci = serializers.CharField(source='station.OACI', read_only=True)
+    station_omm = serializers.CharField(source='station.OMM', read_only=True)
     class Meta : 
         model = Observation
         fields = '__all__'
