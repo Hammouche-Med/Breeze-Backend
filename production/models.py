985fc41f55f55def429a55f7dac1c1ebbd800d73
Hammouche-Med <mhammouche995@gmail.com>
2022-09-17 13:58:19 +0100

    updates && bug fixx

--- production/models.py
+++ production/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 
 class Production (models.Model):
-    name      =       models.CharField(max_length=254)
-    start_t   =       models.TimeField(null=True)
-    end_t     =       models.TimeField(null=True)
-    rate      =       models.IntegerField(null=True)
-    type_obs  =       models.CharField(max_length=254)
-    delay_1t  =       models.IntegerField(null=True)       
-    delay_2t  =       models.IntegerField(null=True)
+    name         =   models.CharField(max_length=254)
+    start_t      =   models.TimeField(null=True)
+    end_t        =   models.TimeField(null=True)
+    rate         =   models.IntegerField(null=True)
+    type_obs     =   models.CharField(max_length=254)
+    is_essential =   models.BooleanField(default=True)
+    full_day     =   models.BooleanField(default=False)
+    delay_1t     =   models.IntegerField(null=True)       
+    delay_2t     =   models.IntegerField(null=True)
 
     def __str__(self):
         return self.name
--- production/serializers.py
+++ production/serializers.py
@@ -1,4 +1,3 @@
-from unittest import expectedFailure
 from rest_framework import serializers
 from .models import Production
 from datetime import datetime
@@ -14,13 +13,20 @@ class ProductionSerializer( serializers.ModelSerializer) :
         t1 = datetime.strptime(str(production.start_t), "%H:%M:%S")
         t2 = datetime.strptime(str(production.end_t), "%H:%M:%S")
         diff = t2-t1
-        return diff.total_seconds()/production.rate 
+        if production.is_essential == True:
+            return round (diff.total_seconds()/production.rate , 0)
+        else:
+            return round( (diff.total_seconds()/production.rate) - (diff.total_seconds()/10800), 0)
+
     def month(self, production):
         now = datetime.now()
         t1 = datetime.strptime(str(production.start_t), "%H:%M:%S")
         t2 = datetime.strptime(str(production.end_t), "%H:%M:%S")
         diff = t2-t1
-        return diff.total_seconds()/production.rate * calendar.monthrange(now.year, now.month)[1]
+        if production.is_essential == True:
+            return round( diff.total_seconds()/production.rate * calendar.monthrange(now.year, now.month)[1], 0)
+        else:
+            return round( (diff.total_seconds()/production.rate * calendar.monthrange(now.year, now.month)[1]) - (diff.total_seconds()/10800 * calendar.monthrange(now.year, now.month)[1]), 0)
 
     
     class Meta : 
